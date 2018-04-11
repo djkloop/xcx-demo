c823d2a3135c1206983053e1ab56c2747939afb8
djkloop <1044032256@qq.com>
2018-04-11 20:09:25 +0800

    update movies

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 App({
   data: {
-    userInfo:{}
+    userInfo: {}
   },
   globalData: {
     g_isPlayingMusic: false,
-    g_currentMusicPostId: void 0
+    g_currentMusicPostId: void 0,
+    g_doubanBaseUrl: "https://douban.uieee.com"
   },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
@@ -27,7 +28,47 @@ App({
       }
     });
   },
+  mixinEvent: {
+    mixinSetNavigationBarColor(opts) {
+      const {ftColor, bg} = opts;
+      wx.setNavigationBarColor({
+        frontColor: ftColor,
+        backgroundColor: bg,
+        animation: {
+          duration: 200,
+          timingFunc: 'easeInOut'
+        }
+      });
+    },
+    mixinSetNavigationBarTitle(opts) {
+      const { title } = opts;
+      wx.setNavigationBarTitle({
+        title
+      });
+    },
+  },
 
+  Util: {
+    wxRequest(opts) {
+      wx.request({
+        url: opts.url,
+        data: opts.data,
+        header: {
+          "Content-Type": "appliction/json"
+        },
+        method: opts.method || 'GET',
+        success:function(res) {
+          opts.success && opts.success(res);
+        },
+        fail: function(res) {
+          opts.fail && opts.fail(res);
+        },
+        complete: function(res) {
+          opts.complete && opts.complete(res);
+        }
+      })
+    }
+  },
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
--- views/movies/movies.js
+++ views/movies/movies.js
@@ -1,65 +1,126 @@
+var appListener = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 
+    const { g_doubanBaseUrl } = appListener.globalData;
+    const { wxRequest } = appListener.Util;
+    appListener.mixinEvent.mixinSetNavigationBarTitle({ title: "小程序电影列表" });
+    var inTheatersUrl = g_doubanBaseUrl + "/v2/movie/in_theaters";
+    var comingSoonUrl = g_doubanBaseUrl + "/v2/movie/coming_soon";
+    var top250Url = g_doubanBaseUrl + "/v2/movie/top250";
+    var _this = this;
+    wxRequest({
+      url: inTheatersUrl,
+      data: {
+        start: 0,
+        count: 3
+      },
+      success: function (res) {
+        var cbData = _this.formatDouBanResData(res.data, 'inTheaters');
+      }
+    })
+
+    wxRequest({
+      url: comingSoonUrl,
+      data: {
+        start: 0,
+        count: 3
+      },
+      success: function (res) {
+        var cbData = _this.formatDouBanResData(res.data, 'comingSoon');
+      }
+    })
+
+    wxRequest({
+      url: top250Url,
+      data: {
+        start: 0,
+        count: 3
+      },
+      success: function (res) {
+        var cbData = _this.formatDouBanResData(res.data, 'top250');
+      }
+    })
+  },
+
+  formatDouBanResData(data, key) {
+    let movies = [];
+    data.subjects.map((subject, index) => {
+      if (subject.title) {
+        subject.title += '-' + index
+      }
+      var temp = {
+        id: subject.id,
+        title: subject.title,
+        average: subject.rating.average,
+        images: subject.images.large
+      }
+      movies.push(temp);
+    });
+    var readyKey = {};
+    readyKey[key] = {
+      movies,
+      showTitle: data.title
+    };
+    this.setData(readyKey);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
